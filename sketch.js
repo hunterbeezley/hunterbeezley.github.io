f6c2002ed34dccb4ce351b0cf1b6470191b9b8d6
Hunter! <38469216+hunterbeezley@users.noreply.github.com>
2024-01-12 20:23:43 -0800

    We're changing "contact" card to "visuals"
    we will start a "visuals experience". Clicking the card will redirect to one visual experience. The user can click "take me somewhere else" and we will redirect to a new random visual experience webpage. Initial commit of this will only have one simple p5.js "experience" we're leaving named "contact"

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup() {
   createBox("Instagram", "https://www.instagram.com/hbeezley/");
 
   // Create a box for the "Contact" hyperlink
-  createBox("Contact", "contact.html");
+  createBox("Visuals", "contact.html");
 
   // Create a box for the "Spotify" hyperlink
   createBox("Spotify", "https://open.spotify.com/playlist/5EuZIHbO7mhNekVu1VXVFu?si=384c7d8aa2324026");
